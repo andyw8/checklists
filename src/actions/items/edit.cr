7ecb59c375c7cb7abcf71686c5ee380639eac458
Andy Waite <andyw8@users.noreply.github.com>
2018-02-04 13:38:07 -0500

    Remove commented-out line

--- src/actions/items/edit.cr
+++ src/actions/items/edit.cr
@@ -2,7 +2,6 @@ class Items::Edit < BrowserAction
   action do
     item = ItemQuery.new.find(id)
 
-    # render EditPage, item_form: ItemForm.new, item: item
     render EditPage, item_form: ItemForm.new(item), item: item
   end
 end
