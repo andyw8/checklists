6914a532574dff63dd0cd25c0b305bd0bfe73fc9
Andy Waite <andyw8@users.noreply.github.com>
2018-02-04 11:50:17 -0500

    Redirect after create

--- src/actions/items/create.cr
+++ src/actions/items/create.cr
@@ -2,10 +2,11 @@ class Items::Create < BrowserAction
   action do
     ItemForm.create(params) do |form, item|
       if item # the item was saved
-        text "Hello #{item.name}"
+        redirect to: Items::Index
       else # the item was not saved
         text "fail"
-       end
+        render NewPage, item_form: form
+      end
     end
   end
 end
