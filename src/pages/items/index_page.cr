a1fb2c00b9a7276cec66b1b03b50f3e63543bd80
Andy Waite <andyw8@users.noreply.github.com>
2018-02-04 11:50:35 -0500

    Styling

--- src/pages/items/index_page.cr
+++ src/pages/items/index_page.cr
@@ -2,10 +2,18 @@ class Items::IndexPage < MainLayout
   needs item_names : ItemQuery
 
   def inner
+    h1 "Items"
+
     ul class: "my-item-list" do
       @item_names.each do |item|
-        li item.name, class: "item-name"
+        link to: Items::Show.with(item.id) do
+          li item.name, class: "item-name"
+
+        end
       end
     end
+
+    link "New item", to: Items::New
+
   end
 end
--- src/pages/items/new_page.cr
+++ src/pages/items/new_page.cr
@@ -2,6 +2,7 @@ class Items::NewPage < MainLayout
   needs item_form : ItemForm
 
   def inner
+    h1 "New Item"
     render_item_form(@item_form)
   end
 
